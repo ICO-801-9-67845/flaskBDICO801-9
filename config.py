5d2561c2cce9a61b759fb96bfefcfaa57931356b
Juan Miguel Trujillo Sanchez <juanmigueltrujillosanchez5@gmail.com>
2026-03-12 13:06:34 -0600

    se agrego la funcionalidad de alumnos

---
+++ config.py
@@ -0,0 +1,11 @@
+import os 
+from sqlalchemy import create_engine  
+
+class Config(object):
+    SECRET_KEY ="claveSecreta"
+    SESSION_COOKIE_SECURE = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://miguel:rootwan1*@127.0.0.1/ico801-9'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Alumnos(db.Model):
+    __tablename__ = 'alumnos'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50))
+    apaterno = db.Column(db.String(50))
+    amaterno = db.Column(db.String(50))
+    edad = db.Column(db.Integer)
+    email = db.Column(db.String(50))
