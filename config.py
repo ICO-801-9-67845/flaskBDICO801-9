7cdcd5d7e2ecf32f5f72946078a2548d222be026
Antonio Montalvo <67845@universidaddeleon.edu.mx>
2026-04-10 10:08:31 -0600

    models

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from sqlalchemy import create_engine
+
+class Config(object):
+    SECRET_KEY = "claveSecreta"
+    SESSION_COOKIE_SECURE = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://montalvo:Jamr130705@127.0.0.1:3306/ico801_9'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
